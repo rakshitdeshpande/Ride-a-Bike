82f96a0bc5714cc9bf8d3611e1bbe7744e9a840e
Rakshit Deshpande <rakshitdeshpande07@gmail.com>
2019-11-09 16:11:55 +0530

    added sigup and login pages

--- app.py
+++ app.py
@@ -6,5 +6,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/signup',methods = ['POST','GET'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    else:
+        return "welcome"
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if request.method=="GET":
+        return render_template("login.html")
+    else:
+        return "welcome"
+
+@app.errorhandler(404)
+def notFound(e):
+    return render_template("404.html"), 404
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000, threaded = True, debug = True)
